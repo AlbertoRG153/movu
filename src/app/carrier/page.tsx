9d860bb449704d9c3ad97e1437d70c0649977a39
Ruth Jimenez <79241546+RuthJimenez309@users.noreply.github.com>
2025-04-07 23:48:25 -0600

    carrier-home

--- src/app/carrier/page.tsx
+++ src/app/carrier/page.tsx
@@ -15,10 +15,16 @@ const CarrierHome = () => {
   return (
     <div className="min-h-screen bg-[#0a2540] text-white flex flex-col items-center justify-center px-4 py-10">
       <h1 className="text-3xl font-bold mb-4">¡Bienvenido , {user?.username || "Conductor"} </h1>
-
+      <Image
+            src="/white.png"
+                alt="Logo"
+                width={180}
+                height={180}
+                layout="fixed"
+                        />
       <p className="text-sm text-gray-300 mb-6">
       Explora tu panel de conductor  y  mejora tu experiencia en carretera.
-      Todo lo que necesitás en un solo lugar.</p>
+      Todo lo que necesitás en un solo lugar Movu.</p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-xl">
         {/* Botón de Iniciar Viaje */}
@@ -45,13 +51,7 @@ const CarrierHome = () => {
       </div>
 
       <div className="mt-10">
-      <Image
-            src="/white.png"
-                alt="Logo"
-                width={180}
-                height={180}
-                layout="fixed"
-                        />
+     
       </div>
     </div>
   );
