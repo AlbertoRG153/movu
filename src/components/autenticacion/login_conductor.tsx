fdb76e8da9ff56ff57bfb3fe8e9d59a43002690c
Ruth Jimenez <79241546+RuthJimenez309@users.noreply.github.com>
2025-04-07 21:58:15 -0600

    le quite lo del usuario y le puse conductor

--- src/components/autenticacion/login_conductor.tsx
+++ src/components/autenticacion/login_conductor.tsx
@@ -164,19 +164,13 @@ export function LoginConductor({
                                     href="/register_conductor"
                                     className="text-emerald-500 hover:text-emerald-600 font-medium"
                                 >
-                                    Crear usuario
+                                    Crear Conductor
                                 </a>
                             </div>
                             <br />
 
                             <Link href="/login">
-                                <Button
-                                    type="submit"
-                                    style={{ backgroundColor: "#2DF1A9" }}
-                                    className="w-full py-6 bg-emerald-400 hover:bg-emerald-500 text-white"
-                                >
-                                    Acceder Modo Cliente
-                                </Button>
+                                
                             </Link>
                         </form>
                     </CardContent>
