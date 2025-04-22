76586638f50a574d801e28da2f59037149237725
Alberto Romero <alberto.romerog27@gmail.com>
2025-04-21 22:09:20 -0500

    Vista de estado de viaje conductor

---
+++ src/app/(core)/carrier/trip_status/page.tsx
@@ -0,0 +1,9 @@
+import TripStatus from "@/components/carrier/trip_status";
+
+export default function TripStatusPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            <TripStatus />
+        </main>
+    );
+}
